98c84b5bb8ed1b437466eb80b91593463a2270a0
dsardinha <15276275+dsardinha@users.noreply.github.com>
2024-08-18 11:59:15 +0100

    Implement display update when operand buttons are clicked

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 let operator, num1, num2;
+let displayValue = '';
+let displayElement = document.querySelector('.display');
+const operandButtons = document.querySelectorAll('.operand');
 
 const operate = (operator, num1, num2) => {
     switch (operator) {
@@ -17,6 +20,17 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+const updateDisplay = btn => {
+    displayValue += btn.textContent;
+    displayElement.textContent = displayValue;
+}
+
+operandButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        updateDisplay(btn);
+    })
+})
 
 console.log(add(10, 5));
 console.log(operate('+', 10, 5));
