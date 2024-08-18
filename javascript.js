3bd251841424881e2846574fdea98d39b6c5e20b
dsardinha <15276275+dsardinha@users.noreply.github.com>
2024-08-18 11:07:40 +0100

    Implement operate function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,28 @@
+let operator, num1, num2;
+
+const operate = (operator, num1, num2) => {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
 const add = (num1, num2) => num1 + num2;
-
 const subtract = (num1, num2) => num1 - num2;
-
 const multiply = (num1, num2) => num1 * num2;
-
 const divide = (num1, num2) => num1 / num2;
 
 console.log(add(10, 5));
+console.log(operate('+', 10, 5));
 console.log(subtract(10, 5));
+console.log(operate('-', 10, 5));
 console.log(multiply(10, 5));
+console.log(operate('*', 10, 5));
 console.log(divide(10, 5));
+console.log(operate('/', 10, 5));
